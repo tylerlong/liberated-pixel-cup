519dcb444d6ffbe9aa4a6dbcc5018556cdc59efa
Tyler Long <tyler4long@gmail.com>
2020-05-26 16:30:31 -0700

    Overlay

--- test/path.spec.ts
+++ test/path.spec.ts
@@ -4,14 +4,14 @@ import LPC from '../src/index';
 jest.setTimeout(64000);
 
 describe('default', () => {
-  test('default', async () => {
+  test('path', async () => {
+    expect(LPC.body().male().dark().path()).toEqual(['body', 'male', 'dark']);
+
     expect(LPC.hair().male().long().blue().path()).toEqual([
       'hair',
       'male',
       'long',
       'blue',
     ]);
-
-    expect(LPC.body().male().dark().path()).toEqual(['body', 'male', 'dark']);
   });
 });
